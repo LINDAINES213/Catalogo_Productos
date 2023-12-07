80ac6d5f38ec8a8868c01fde9c39da05a1a2cdfe
LINDA JIMÉNEZ <jim21169@uvg.edu.gt>
2023-12-07 12:39:02 -0600

    menu finalizado y menu movil

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { RiMenu3Fill, RiUser3Line, RiAddLine, RiLightbulbLine } from 'react-icons/ri'
 import Sidebar from './components/shared/Sidebar'
 
 function App() {
@@ -5,6 +6,21 @@ function App() {
   return (
       <div className='bg-[#262837] w-full min-h-screen'>
         <Sidebar/>
+        {/* Menu Movil */}
+        <nav className='bg-[#1F1D2B] lg:hidden fixed w-full bottom-0 left-0 text-3xl text-white p-4 px-8 flex items-center justify-between rounded-tl-xl rounded-tr-xl'>
+          <button>
+            <RiUser3Line/>
+          </button>
+          <button>
+            <RiAddLine/>
+          </button>
+          <button>
+            <RiLightbulbLine/>
+          </button>
+          <button>
+            <RiMenu3Fill/>
+          </button>
+        </nav>
       </div>
   )
 }
